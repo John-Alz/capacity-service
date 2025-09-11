7bb89e61c98e2be8432f928cac8a48dc949e1943
John Angel <jairoanngelll@gmail.com>
2025-09-11 15:20:05 -0500

    first commit setup project

---
+++ src/main/java/com/onclass/capacity_service/CapacityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.onclass.capacity_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapacityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapacityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onclass/capacity_service/CapacityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onclass.capacity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapacityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
