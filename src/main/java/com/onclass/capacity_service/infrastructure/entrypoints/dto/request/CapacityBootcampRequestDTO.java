2511b7d44f5fbcd340d32dc17ea675899c5c3290
John Angel <jairoanngelll@gmail.com>
2025-09-14 21:46:19 -0500

    feat: endpoint para guardar relacion entre capacidad y bootcamp

---
+++ src/main/java/com/onclass/capacity_service/infrastructure/entrypoints/dto/request/CapacityBootcampRequestDTO.java
@@ -0,0 +1,6 @@
+package com.onclass.capacity_service.infrastructure.entrypoints.dto.request;
+
+import java.util.List;
+
+public record CapacityBootcampRequestDTO(List<Long> capacitiesId) {
+}
---
+++ src/main/java/com/onclass/capacity_service/infrastructure/entrypoints/handler/CapacityBootcampHandler.java
@@ -0,0 +1,32 @@
+package com.onclass.capacity_service.infrastructure.entrypoints.handler;
+
+import com.onclass.capacity_service.domain.api.CapacityBootcampServicePort;
+import com.onclass.capacity_service.infrastructure.entrypoints.dto.request.CapacityBootcampRequestDTO;
+import com.onclass.capacity_service.infrastructure.entrypoints.dto.response.SuccessReponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class CapacityBootcampHandler {
+
+    private final CapacityBootcampServicePort capacityBootcampServicePort;
+
+    public Mono<ServerResponse> saveCapacityBootcamp(ServerRequest request) {
+        Long bootcampId = Long.valueOf(request.pathVariable("bootcampId"));
+        return request.bodyToMono(CapacityBootcampRequestDTO.class)
+                .flatMap(dto -> capacityBootcampServicePort.addAll(bootcampId, dto.capacitiesId()))
+                .then(Mono.defer(() -> ServerResponse
+                        .created(URI.create("/capacity-bootcamps/" + bootcampId))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .bodyValue(new SuccessReponseDTO("Relacion Capacity Bootcamp Creado.", LocalDateTime.now()))));
+    }
+
+}
